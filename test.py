da5be3a01f878b39d7c76b3abb9fbf738f02c65b
Loïc Estève <loic.esteve@ymail.com>
2024-09-13 16:51:10 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import pprint
+import sys
+import platform
+
+import lightgbm
+import torch
+import threadpoolctl
+
+print('version: ', sys.version, flush=True)
+print('platform: ', platform.platform(), flush=True)
+pprint.pprint(threadpoolctl.threadpool_info())
+
+print('before torch tensor', flush=True)
+t = torch.ones(200_000)
+print('after torch tensor', flush=True)
